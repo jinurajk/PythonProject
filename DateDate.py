3de87a7113300327b613ee7d607310912050159b
jinurajk <jinurajk@yahoo.co.in>
2017-02-19 21:08:30 +0100

    file modified

--- DateDate.py
+++ DateDate.py
@@ -2,7 +2,11 @@ import calendar
 
 cal = calendar.month(2016,7)
 
+<<<<<<< HEAD
 print ("Calendar for July ......")
+=======
+print ("Calendar for July .//.././/.")
+>>>>>>> Release1
 print ("Calendar of 2016 July ", cal)
 
 print (calendar.calendar(2000,w=3,l=1,c=12))
